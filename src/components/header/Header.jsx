1058c1d74ddbd284aee1ba6c6f58f913430e7bba
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-10-18 19:35:09 -0300

    reubicacion de imagenes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import './Header.css'
+import imgLogo from '../../assets/images/logoAtad.jpg'
 
 function Header() {
     return (
         <>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos labore perferendis dolorum dignissimos fugiat aliquam adipisci minus! Voluptatibus fugiat quidem dolore rerum obcaecati vitae soluta alias accusantium atque praesentium!</p>
-        <img src="../../assets/images/logoAtad.jpg" alt="Logo ATAD" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos labore perferendis dolorum dignissimos fugiat aliquam adipisci minus! Voluptatibus fugiat quidem dolore rerum obcaecati vitae soluta alias accusantium atque praesentium!</p>
+          <img src={imgLogo} className='header_logo' alt="Logo ATAD" />
         </>
     )
 }
