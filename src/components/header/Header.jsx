65a2e5686acb89f9b5797795d928c89e20988c15
gabrieladel <gabi23@live.com.ar>
2023-10-22 19:20:55 -0300

    complete el header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,33 @@
 import './Header.css'
 import imgLogo from '../../assets/images/logoAtad.jpg'
-import Button from '../Button/Button'
 
 function Header() {
     return (
         <>
-        <div className='header_container'>
-        
-          <img src={imgLogo} className='header_logo' alt="Logo ATAD" />
-          <div>
-          <Button text='Nosotros'/>
-          <Button text='Eventos'/>
-          <Button text='Proyectos'/>
-          <Button text='Contacto'/>
-          
-          </div>
-          </div>
+          <nav className="navbar navbar-expand-lg navbar-white bg-light fixed-top">
+            <div className="container">
+            <img src={imgLogo} className='header_logo' width="30" height="24" />
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="#nosotros">Nosotros</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#eventos">Eventos</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#proyectos">Proyectos</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#contacto">Contacto</a>
+                  </li>
+                </ul>
+              </div>
+              </div>
+          </nav>
         </>
     )
 }
