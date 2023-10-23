bf9b7c6a0f2acbcd4f6d8782c7a5015f47773e9c
monica <monicagjofre@gmail.com>
2023-10-23 15:40:35 -0300

    className icons

--- src/components/header/footer/Footer.jsx
+++ src/components/header/footer/Footer.jsx
@@ -10,11 +10,11 @@ function Footer() {
               className='btn btn-link btn-floating btn-lg text-dark m-1'>
               <i className="bi bi-facebook"></i>
               <i className="bi bi-instagram"></i>
-              <i class="bi bi-whatsapp"></i>
-              <i class="bi bi-envelope-at"></i>
+              <i className="bi bi-whatsapp"></i>
+              <i className="bi bi-envelope-at"></i>
             </ul>
 
-            <div className='ubicacion text-center text-dark p-3 '><i class="bi bi-geo-alt"></i>
+            <div className='ubicacion text-center text-dark p-3 '><i className="bi bi-geo-alt"></i>
               Encontranos en:
   
               <a className='text-dark href="#" '> Cnel. Brandsen 491, Tandil, Provincia de Buenos Aires</a>
