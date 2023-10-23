25209b8f6cac8e6623c9bc48f27c64fcb8a9f57a
monica <monicagjofre@gmail.com>
2023-10-23 15:35:29 -0300

    importe el Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
 import './App.css'
 import Header from './components/header/Header'
 import ImageCircle from './components/main/ImageCircle'
+import Footer from './components/header/footer/Footer'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <Header />
-      
+      <Footer />
       <br />
       <br />
       <br />
