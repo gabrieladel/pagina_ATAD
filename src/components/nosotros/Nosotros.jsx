4b72366ad2a4b7a2c966126ff14b48482aec4197
gabrieladel <gabi23@live.com.ar>
2023-10-25 19:03:13 -0300

    cambios en nosotros

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -6,15 +6,20 @@ export default function Nosotros() {
     return (
         <>
         <div>
+          <div>
         <h1>ATAD</h1>
            <h3>Asociación Tandilense de Ayuda al Discapacitado </h3>
-           <p>
-           Es una institución educativa  privada fundada en 1982, de modalidad especial.
-               Abarca los niveles:
-                                inicial
-                                primario
-                                CFI </p>
-                                <img src={ imagen } alt="ATAD" width="350" height="270"/> 
+           <p>Es una institución educativa  privada fundada en 1982, de modalidad especial.
+               Abarca los niveles: </p>
+         <ul>
+          <li>Inicial</li>
+          <li>Primario</li>
+          <li>CFI</li>
+         </ul>
+         </div>
+         <div>
+        <img src={ imagen } alt="ATAD" width="350" height="270"/> 
+        </div>
 <div className="card">
   <img src={ imagen_atdi } alt="ATAD" className="card-img-top" width="360" height="280" />
   <div className="card-body">
