d2868e15cbc3791ca3b83b0851ee03194058f559
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-10-27 20:15:10 -0300

    espaciado superior general

--- src/components/social/Face.jsx
+++ src/components/social/Face.jsx
@@ -1,13 +1,36 @@
+import "./Face.css";
 
-
-import './Face.css'
-
-export default function Face(params) {
-    return (
-        <div class="fb-page" 
+export default function Face() {
+  return (
+    <>
+      <div
+        class="fb-page"
         data-tabs="timeline,events,messages"
-          data-href="https://www.facebook.com/profile.php?id=100057622172867"
-  data-width="380" 
-  data-hide-cover="false"></div>
-    )
+        data-href="https://www.facebook.com/profile.php?id=100057622172867"
+        data-width="380"
+        data-hide-cover="false"
+      ></div>
+
+      <div
+        class="fb-page"
+        data-href="https://www.facebook.com/profile.php?id=100057622172867"
+        data-tabs="timeline, events, messages"
+        data-width="380"
+        data-height="500"
+        data-small-header="false"
+        data-adapt-container-width="true"
+        data-hide-cover="false"
+        data-show-facepile="true"
+      >
+        <blockquote
+          cite="https://www.facebook.com/profile.php?id=100057622172867"
+          class="fb-xfbml-parse-ignore"
+        >
+          <a href="https://www.facebook.com/profile.php?id=100057622172867">
+            Un lugar para Atad
+          </a>
+        </blockquote>
+      </div>
+    </>
+  );
 }
