ab1120549315d96c140e88de4fe21a55b4a7356e
gabrieladel <gabi23@live.com.ar>
2023-10-30 18:44:47 -0300

    alinee las imagenes y agregue Logo

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -1,43 +1,53 @@
-import './Nosotros.css'
-import imagen from '../../assets/images/ATAD-In.jpg'
-import imagen_atdi from '../../assets/images/atdi_img.jpg'
+import "./Nosotros.css";
+import imagen from "../../assets/images/ATAD-In.jpg";
+import imagen_atdi from "../../assets/images/atdi_img.jpg";
+import imgLog from "../../assets/images/logoAtad.jpg";
 
 export default function Nosotros() {
-    return (
-        <>
-        <section id="nosotros">
-        <div> 
-            <div className='conteiner'>
-              <div className='conteiner-text'>
-              <h1>ATAD</h1>
-              <h5>Asociación Tandilense de Ayuda al Discapacitado </h5>
-              <p>Es una institución educativa  privada fundada en 1982, de modalidad especial.</p>
-                  <h6>Abarca los niveles: </h6>
-              <ul>
-                <li>*Inicial</li>
-                <li>*Primario</li>
-                <li>*CFI</li>
-              </ul>
-             </div>
-            <div className='imagen1'>
-            <img src={ imagen } alt="ATAD" width="350" height="270"/> 
+  return (
+    <>
+      <section id="nosotros">
+        <div className="conteiner">
+          <div className="conteiner-text">
+            <img src={imgLog} className="log" />
+            <h1>ATAD</h1>
+            <h5>Asociación Tandilense de Ayuda al Discapacitado </h5>
+            <p>ESCUELA DE EDUCACION ESPECIAL DIEGEP N 1963</p>
+            <h6>Abarca los niveles: </h6>
+            <ul>
+              <li>*Inicial</li>
+              <li>*Primario</li>
+              <li>*CFI</li>
+            </ul>
+            <a href="#proyectos" className="btn btn-primary btn-lg">
+              Nuestros Proyectos
+            </a>
+          </div>
+          <div className="conteiner-2">
+            <div className="imagen1">
+              <img src={imagen} alt="ATAD" width="390" height="230" />
             </div>
-            </div>
-           <div className='conteiner-2'>
-            <a href="#proyectos" className="btn btn-primary btn-lg w-33">Nuestros Proyectos</a>
-            <div className="card">
-              <img src={ imagen_atdi } alt="ATAD" className="card-img-top" width="360" height="280" />
+            <div className="card border-0">
+              <img
+                src={imagen_atdi}
+                alt="ATDI"
+                className="card-img-top rounded-0"
+                width="360"
+                height="230"
+              />
               <div className="card-body">
                 <h5 className="card-title">ATDI</h5>
-                <p className="card-text">Atención temprana del desarrollo infantil.</p>
-                <a href="#" className="btn btn-primary">Para mas info</a>
+                <p className="card-text">
+                  Atención temprana del desarrollo infantil (45 dias a 3 años).
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Para mas info
+                </a>
               </div>
+            </div>
           </div>
-          </div>  
-          </div> 
-          </section>              
-        </>
-    )
+        </div>
+      </section>
+    </>
+  );
 }
-
- 
