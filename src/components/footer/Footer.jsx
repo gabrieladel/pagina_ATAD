20520a98e8c0de0d77419fa486aff1b98e5edddf
gabrieladel <gabi23@live.com.ar>
2023-11-04 18:56:01 -0300

    acomode el ancho y agregue los href

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,30 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className='container w-100'> 
-
-      <footer className='container-footer bg-light'>
-       
-          <div className='lista'>
-            < ul className='iconos'>
-              <i className="bi bi-facebook"></i>
-              <i className="bi bi-instagram"></i>
-              <i className="bi bi-whatsapp"></i>
-              <i className="bi bi-envelope-at"></i>
+    <>
+      <nav className="navbar-expand-lg bg-light fixed">
+        <div className="container">
+          <div className="lista">
+            <ul className="iconos">
+              <a href="https://www.facebook.com/profile.php?id=100057622172867&locale=es_LA" target="_blanck" title="Facebook"><i className="bi bi-facebook"></i></a>
+              <a href="https://www.instagram.com/atad1963/" target="_blanck" title="Instagram"><i className="bi bi-instagram"></i></a>
+               <a href="https://www.facebook.com/groups/614288792001824/?ref=share" target="_blanck" title="Whatsapp"><i className="bi bi-whatsapp"></i></a>
+               <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=atad1963@gmail.com" target="_blanck" title="Gmail"><i className="bi bi-envelope-at"></i></a>
             </ul>
 
-            <div className='ubicacion text-center text-dark p-3 '><i className="bi bi-geo-alt"></i>
+            <div className="ubicacion text-center text-dark p-3 ">
+              <i className="bi bi-geo-alt"></i>
               Encontranos en:
-  
-              <a className='text-dark href="#" '> Cnel. Brandsen 491, Tandil, Provincia de Buenos Aires</a>
+              <a className='text-dark href="#" '>
+                {" "}
+                Cnel. Brandsen 498, Tandil, Provincia de Buenos Aires
+              </a>
             </div>
-          
+          </div>
         </div>
-      </footer>
-    </div>
-  )
+      </nav>
+      </>
+  );
 }
-export default Footer
+export default Footer;
