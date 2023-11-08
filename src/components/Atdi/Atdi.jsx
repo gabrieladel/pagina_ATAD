24a758533b598499a7ff9b2eb33c1163c2c30217
agustin.achagaperez <achagaperezagustin2015@gmail.com>
2023-11-08 19:24:18 -0300

    Se volvió a agregar información de sección ATDI para agregar a Main.

---
+++ src/components/Atdi/Atdi.jsx
@@ -0,0 +1,22 @@
+import './Atdi.css';
+
+ function Atdi(){
+    return (
+        <div className='container  my-5'>
+            <section className='container-atadi'> 
+                <div className='Atdi-info'>
+                 <h1>ATDI</h1>
+                 <p>
+                   ATDI (Atencion Temprana Del desarrollo Infantil 45 dias a 3 años).
+                 • Propiciar el derecho a la educación desde el momento del nacimiento, la expansión y el impacto del Modelo social de la discapacidad para beneficiar el desarrollo de las infancias.
+                 • Realizar una valoración pedagógica que detecte las necesidades de atención. 
+                 • Planificar acciones integrales que aporten al cuidado, crianza y educación de los bebes, niños y niñas.
+                 • Potenciar el desarrollo de los bebes, niños y niñas, a través de la intervención organizada y personalizada por medio de trayectorias educativas integrales.
+                 • Realizar un trabajo compartido con las familias. 
+                 </p>
+                </div>
+             </section>
+        </div>
+    )
+ }
+ export default Atdi
