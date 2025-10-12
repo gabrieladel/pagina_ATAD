bef47ad5d7eb7f3f0c068128b3d1382e14fa066f
gabrieladel <gabi23@live.com.ar>
2025-10-12 20:17:16 -0300

    instale nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
 const express = require ('express');
 const app = express();
 
+const PORT = 3000; 
+
 app.get('/', (req, res) =>{
-    res.send('hola mundo')
-}
-
-)
-
-app.listen(6500,() =>{
-    console.log('Servidor activo')
+    res.send('hola mundo');
 }
 
 );
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
