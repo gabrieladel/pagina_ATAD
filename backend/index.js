ab74f3d4643938cc00295bb0a3985057a49c6fee
gabrieladel <gabi23@live.com.ar>
2025-10-23 20:09:18 -0300

    cambios

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-const express = require ('express');
+const express = require('express');
 const app = express();
+require('dotenv').config();
 
-app.use(require("./src/routes/main.router"));
-app.use(require("./src/routes/noticias.router"))
+app.use(express.json());
+app.use('/api/noticias', require("./src/routes/noticias.router"));
+app.use('/', require("./src/routes/main.router"));
 
-const PORT = 3000; 
-
-
+const PORT = 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
