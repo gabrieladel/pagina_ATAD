29c8d907d9668bbc31836c01d69ea81d7486eabf
gabrieladel <gabi23@live.com.ar>
2025-10-28 15:54:07 -0300

    agregue nuevas app

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use('/api/noticias', require("./src/routes/noticias.router"));
+app.use('/api/contactos', require("./src/routes/contactos.router"));
+app.use('/api/usuarios', require("./src/routes/usuarios.router"));
+app.use('/api/actividades', require("./src/routes/actividades.router"));
+app.use('/api/roles', require("./src/routes/roles.router"));
 app.use('/', require("./src/routes/main.router"));
 
 const PORT = 3000;
