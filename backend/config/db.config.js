1d18cc5918413f0fd00e278f72d5799c0e99279d
gabrieladel <gabi23@live.com.ar>
2025-11-09 00:54:44 -0300

    se configuro la base de datos

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,28 @@
-module.exports = {
-  HOST: "127.0.0.1",
-  USER: "root",
-  PASSWORD: "",
-  DB: "web_atad"
-};
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,     
+  process.env.DB_USER,     
+  process.env.DB_PASS,     
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    port: process.env.DB_PORT || 3306,
+    logging: false,
+  }
+);
+
+// Test de conexión
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conexión exitosa a MySQL con Sequelize');
+  } catch (error) {
+    console.error('❌ Error al conectar con Sequelize:', error);
+  }
+})();
+
+module.exports = sequelize;
