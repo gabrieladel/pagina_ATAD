54aba0ac035b3961bfde269b9fbaba763b827dd0
gabrieladel <gabi23@live.com.ar>
2025-11-11 22:17:25 -0300

    .

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -146,7 +146,7 @@ const Dashboard = () => {
                     </div>
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                            <h1 className="h2">Dashboard</h1>
+                            <h1 className="h2">Panel administrativo</h1>
                         </div>
                         {renderizarContenido()} {/* Llamamos a la función para renderizar el contenido */}
                     </main>
