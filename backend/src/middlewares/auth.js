a6be9f19448d08f48d6ef186b9813bef7531eb6c
gabrieladel <gabi23@live.com.ar>
2025-11-14 21:31:57 -0300

    cambios

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -4,13 +4,20 @@ const jwtSecret = process.env.JWT_SECRET;
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (!token) return res.sendStatus(401);
+
+  if (!token) {
+    return res.status(401).json({ error: "Token requerido" });
+  }
 
   jwt.verify(token, jwtSecret, (err, usuario) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      return res.status(403).json({ error: "Token inválido o expirado" });
+    }
+
     req.usuario = usuario;
     next();
   });
 };
 
 module.exports = authenticateToken;
+
