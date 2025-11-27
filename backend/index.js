c0fd9b5f4fc5359929c6a38046c136fc995f63f2
gabrieladel <gabi23@live.com.ar>
2025-11-27 01:01:54 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -152,5 +152,8 @@ sequelize.sync()
   .catch(err => console.error('Error al sincronizar la base de datos:', err));
 
   // PORT (Railway usa process.env.PORT)
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Servidor en" + PORT));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor escuchando en el puerto " + PORT);
+});
+
