3352a89d9689a74bac17a244a21ecb3b2ef00abd
gabrieladel <gabi23@live.com.ar>
2025-11-27 17:33:25 -0300

    cambios dep

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  base: "./",   // <<< ESTO ES LO QUE FALTABA
+  build: {
+    outDir: "dist",
   },
-})
+});
