b5d96bc32fa06d51e1741444d83040a89c73879f
gabrieladel <gabi23@live.com.ar>
2025-11-28 02:12:32 -0300

    cambios para rai

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, '../backend/public'), // Build va a backend/public
-    emptyOutDir: true,
+    outDir: 'dist'
   },
+  base: '/'
 });
