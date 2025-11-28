e89b9aba14d63661b1a31ecf8e1c51a4c0268604
gabrieladel <gabi23@live.com.ar>
2025-11-27 22:57:40 -0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // ¡Añade esta línea!
-  // El valor debe ser el nombre de tu repositorio, rodeado de barras inclinadas.
-  base: '/pagina_ATAD/', 
+  base: '/pagina_ATAD/'
 })
