ea3fc3e0f7378fbfe80144112af722a5aaaa065b
gabrieladel <gabi23@live.com.ar>
2025-11-27 22:41:51 -0300

    Guardando cambios antes de cambiar de rama

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,26 +1,26 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,     
-  process.env.DB_USER,     
-  process.env.DB_PASS,     
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql',
-    port: process.env.DB_PORT || 3306,
-    logging: false,
+    logging: console.log,  // <-- ACTIVAR TEMPORALMENTE
   }
 );
-
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log(' Conexión exitosa a MySQL con Sequelize');
+    console.log("Conexión exitosa a MySQL en Railway");
   } catch (error) {
-    console.error(' Error al conectar con Sequelize:', error);
+    console.error("Error al conectar a MySQL:", error);
   }
 })();
 
 module.exports = sequelize;
+
